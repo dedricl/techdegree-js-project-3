ab579d493b210efc5f1aab04b41e85e71721fe78
Dedric Leftwich <dedricleftwich@gmail.com>
2019-05-18 22:26:29 -0400

    Added a text filed to job role and js code to hide and show it

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 
 //When documment loads up or user refresh the window the Name text field is selected first by default.
 $('#name').focus();
+
+//Hide job role text field if other is not selected
+$('#other-title').hide();
+
+
+//Show job role text field when other is selected
+ $('#title').on('change',function(){
+   if($(this).val() === 'other'){
+     $('#other-title').show();
+   }else{
+     $('#other-title').hide();
+   }
+
+});
